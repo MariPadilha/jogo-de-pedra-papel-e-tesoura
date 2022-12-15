3a977bdb2e3be18242dabf4c6fba7aa466889f30
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2022-12-14 22:11:23 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -4,22 +4,41 @@ print('''Regras do pedra, papel e tesoura:
     pedra vs tesoura -> vitória da pedra
     papel vs tesoura-> vitória da tesoura.''')
 print('-='*20)
+VC = VE = 0
 while True:
     lista = ['tesoura','papel','pedra']
     computador = choice(lista)
     vc = input('digite sua escolha (pedra, papel ou tesoura): ')
     print('-=' * 20)
-    print(f'           Computador escolheu {computador}')
+    print(f'      Computador escolheu {computador}')
     print('-=' * 20)
     print('resultado:'.upper())
-    print('*'*10)
+    print('*'*15)
     if vc == computador:
         print('Deu empate'.upper())
     elif vc == 'tesoura':
-        print('vc perdeu, boa sorte na próxima') if computador == 'pedra' else print('vc venceu')
+        if computador == 'pedra':
+            VC += 1
+            print('vc perdeu, boa sorte na próxima')
+        else:
+            VE +=1
+            print('vc venceu')
     elif vc == 'pedra':
-        print('vc perdeu, boa sorte na próxima') if computador == 'papel' else print('vc venceu')
+        if computador == 'papel':
+            VC += 1
+            print('vc perdeu, boa sorte na próxima')
+        else:
+            VE += 1
+            print('vc venceu')
     elif vc == 'papel':
-        print('vc perdeu, boa sorte na próxima') if computador == 'tesoura' else print('vc venceu')
+        if computador == 'tesoura':
+            VC += 1
+            print('vc perdeu, boa sorte na próxima')
+        else:
+            VE += 1
+            print('vc venceu')
+    print(f' O computador já ganhou {VC} vez(es) e você já ganhou {VE} vez(es)')
+    print('0-0-'*8)
     if input('vc quer continuar? [s/n] ').lower() == 'n':
         print('Muito obrigada por jogar, esperamos que tenha gostado!!')
+        break
